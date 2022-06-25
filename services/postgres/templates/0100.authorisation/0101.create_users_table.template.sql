82eff9caaaf1c8c968c43f2fcc761ddf27568fca
sam <sam@mollybet.com>
2022-06-25 08:56:27 +0100

    fix: Mark username as unique in db

--- services/postgres/templates/0100.authorisation/0101.create_users_table.template.sql
+++ services/postgres/templates/0100.authorisation/0101.create_users_table.template.sql
@@ -1,7 +1,7 @@
 \connect ${postgres.authorisation.name};
 CREATE TABLE IF NOT EXISTS ${postgres.authorisation.tables.users} (
     id             SERIAL PRIMARY KEY,
-    username       TEXT NOT NULL,
+    username       TEXT NOT NULL UNIQUE,
     password       TEXT NOT NULL,
     name           TEXT NOT NULL,
     scopes         TEXT[]
