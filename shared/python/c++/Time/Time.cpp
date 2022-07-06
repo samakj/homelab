3212a80f490b89720b991888caef280efd3d3808
sam <sam@mollybet.com>
2022-07-06 18:18:38 +0100

    fix: Add time library

---
+++ shared/python/c++/Time/Time.cpp
@@ -0,0 +1,80 @@
+#include "Time.h"
+
+unsigned long Homelab::Time::millisDiff(unsigned long start, unsigned long end)
+{
+    if (end >= start)
+        return end - start;
+    unsigned long startOffset = std::numeric_limits<unsigned long>::max() - start;
+    return end + startOffset;
+};
+
+unsigned long Homelab::Time::millisSince(unsigned long start)
+{
+    return Homelab::Time::millisDiff(start, millis());
+};
+
+std::string Homelab::Time::getIsoTimestamp()
+{
+    char buffer[23];
+    time_t tm = time(nullptr);
+    strftime(buffer, 23, "%FT%TZ", gmtime(&tm));
+    return std::string(buffer);
+};
+
+std::string Homelab::Time::formatTime(const char *format)
+{
+    char buffer[64];
+    time_t tm = ::time(nullptr);
+    strftime(buffer, sizeof(buffer), format, gmtime(&tm));
+    return std::string(buffer);
+};
+
+// NTP
+
+std::string Homelab::Time::NTP::server = "uk.pool.ntp.org";
+uint16_t Homelab::Time::NTP::maxWait = 20000;
+std::vector<Homelab::Time::NTP::ConnectCallback> Homelab::Time::NTP::connectCallbacks = {};
+bool Homelab::Time::NTP::_isConnecting = false;
+
+bool Homelab::Time::NTP::isConnecting() { return Homelab::Time::NTP::_isConnecting; };
+bool Homelab::Time::NTP::isConnected() { return time(nullptr) > 1616000000; };
+
+void Homelab::Time::NTP::setServer(std::string server) { Homelab::Time::NTP::server = server; };
+void Homelab::Time::NTP::setMaxWait(uint16_t maxWait) { Homelab::Time::NTP::maxWait = maxWait; };
+
+void Homelab::Time::NTP::addConnectCallbak(Homelab::Time::NTP::ConnectCallback callback)
+{
+    Homelab::Time::NTP::connectCallbacks.push_back(callback);
+};
+
+void Homelab::Time::NTP::connect(bool force)
+{
+    if (!Homelab::Wifi::isConnected())
+        Homelab::Logger::warn("No internet connection, skiping NTP.");
+    else if (force || (!Homelab::Time::NTP::isConnecting() && !Homelab::Time::NTP::isConnected()))
+    {
+        Homelab::Time::NTP::_isConnecting = true;
+        configTime(0, 0, Homelab::Time::NTP::server.c_str());
+    }
+};
+
+void Homelab::Time::NTP::loop()
+{
+    if (!Homelab::Time::NTP::isConnected() && !Homelab::Time::NTP::isConnecting())
+        connect();
+
+    if (Homelab::Time::NTP::isConnected() && Homelab::Time::NTP::isConnecting())
+    {
+        Homelab::Logger::infof(
+            "Synced with '%s'. Time: %s.\n",
+            Homelab::Time::NTP::server.c_str(),
+            Homelab::Time::getIsoTimestamp().c_str());
+
+        for (
+            Homelab::Time::NTP::ConnectCallback callback :
+            Homelab::Time::NTP::connectCallbacks)
+            callback();
+
+        Homelab::Time::NTP::_isConnecting = false;
+    }
+};
---
+++ shared/python/c++/Time/Time.h
@@ -0,0 +1,46 @@
+
+#ifndef _Homelab_Time_h
+#define _Homelab_Time_h
+
+#include <Arduino.h>
+#include <string>
+#include <functional>
+#include <time.h>
+#include "../Logger/Logger.h"
+
+namespace Homelab::Wifi
+{
+    bool isConnected();
+}
+
+namespace Homelab::Time
+{
+    unsigned long millisDiff(unsigned long start, unsigned long end);
+    unsigned long millisSince(unsigned long start);
+    std::string getIsoTimestamp();
+    std::string formatTime(const char *format);
+
+    namespace NTP
+    {
+        typedef std::function<void()> ConnectCallback;
+
+        extern std::string server;
+        extern uint16_t maxWait;
+        extern std::vector<ConnectCallback> connectCallbacks;
+        extern bool _isConnecting;
+
+        bool isConnecting();
+        bool isConnected();
+
+        void setServer(std::string server);
+        void setMaxWait(uint16_t maxWait);
+
+        void addConnectCallbak(ConnectCallback callback);
+
+        void connect(bool force = false);
+        void loop();
+    }
+
+};
+
+#endif
