7d019d4b988e76c8cae9d3036fde2ec0f9c5842e
samakj <samakj@live.co.uk>
2022-07-10 14:01:31 +0100

    fix: Add libraries to root namespace

--- shared/c++/Homelab.h
+++ shared/c++/Homelab.h
@@ -1,7 +1,13 @@
 #ifndef Homelab_h
 #define Homelab_h
 
+#include "Logger/Logger.h"
+#include "OTA/OTA.h"
+#include "Peripherals/Peripherals.h"
+#include "Sensors/Sensors.h"
+#include "Time/Time.h"
 #include "Utils/Utils.h"
+#include "Wifi/Wifi.h"
 
 namespace Homelab
 {
