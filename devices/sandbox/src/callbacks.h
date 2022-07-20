98fc088fe2b7e8367b2b1f681dc5cee4381acfe6
sam <sam@mollybet.com>
2022-07-20 14:48:37 +0100

    fix: Add full device code to sandbox

--- devices/sandbox/src/callbacks.h
+++ devices/sandbox/src/callbacks.h
@@ -1,16 +1,39 @@
 #ifndef callbacks_h
 #define callbacks_h
 
-#include <OTA/OTA.h>
-#include <Time/Time.h>
-#include <Wifi/Wifi.h>
+#include <Homelab.h>
 
-namespace Callbacks
+#include "config.h"
+#include "globals.h"
+#include "tags.h"
+
+void onWifiConnect(std::string ssid)
 {
-    namespace Wifi
-    {
+    Homelab::OTA::setup(HOSTNAME, OTA_PASSWORD);
+    Homelab::Time::NTP::connect();
+    Homelab::Server::setup();
+};
 
-    }
+void onWifiSSIDChange(std::string ssid)
+{
+    Homelab::Server::sendReport(ssid.c_str(), "name", WIFI_SSID_TAGS);
+};
+
+void onWifiStrengthChange(float strength)
+{
+    Homelab::Server::sendReport(strength * 100, "strength", WIFI_STRENGTH_TAGS);
+};
+
+void onNTPConnect(){};
+
+void onDHTTemperatureChange(float temperature)
+{
+    Homelab::Server::sendReport(temperature, "temperature", DHT_TEMPERATURE_TAGS);
+}
+
+void onDHTHumidityChange(float humidity)
+{
+    Homelab::Server::sendReport(humidity, "humidity", DHT_HUMIDITY_TAGS);
 }
 
 #endif
--- devices/sandbox/src/main.cpp
+++ devices/sandbox/src/main.cpp
@@ -3,15 +3,42 @@
 
 #include <Homelab.h>
 
+#include "callbacks.h"
+#include "config.h"
+#include "globals.h"
+
 void setup()
 {
     Homelab::Logger::info("---- Running setup ----");
 
+    // // Set properties
+    Homelab::Time::NTP::setServer(NTP_SERVER);
+    // Homelab::Wifi::addConnectCallback(onWifiConnect);
+    // Homelab::Wifi::addSSIDChangeCallback(onWifiSSIDChange);
+    // Homelab::Wifi::addStrengthChangeCallback(onWifiStrengthChange);
+    // Homelab::Time::NTP::addConnectCallbak(onNTPConnect);    
+    // DHTSensor.addTemperatureCallback(onDHTTemperatureChange);
+    // DHTSensor.addHumidityCallback(onDHTHumidityChange);
+
+    // Start wifi connection
+    // Homelab::Wifi::connect(WifiCredentials, HOSTNAME, IP_ADDRESS);
+
+    // // Setup dht sensor
+    DHTSensor.setup();
     Homelab::Logger::info("---- Setup complete ----");
 }
 
+unsigned long lastHeartbeat = 0;
+
 void loop()
 {
-    Serial.println(Homelab::Time::formatTime("%T").c_str());
-    delay(1000);
+    if (Homelab::Time::millisSince(lastHeartbeat) > 15000)
+    {
+        Homelab::Logger::info("Heartbeat");
+        lastHeartbeat = millis();
+    }
+    // Homelab::Wifi::loop();
+    Homelab::Server::loop();
+    Homelab::OTA::loop();
+    DHTSensor.loop();
 }
