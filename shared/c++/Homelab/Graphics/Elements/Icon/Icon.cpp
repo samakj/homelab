86b47b76b3ee31f9e7d90e098b4e01bbc1ad2f3f
samakj <samakj@live.co.uk>
2022-09-02 12:24:16 +0100

    fix: Add element for specifically displaying single characters

---
+++ shared/c++/Homelab/Graphics/Elements/Icon/Icon.cpp
@@ -0,0 +1,70 @@
+#include "Text.h"
+
+Homelab::Graphics::Elements::Text::Text(
+    int16_t x,
+    int16_t y,
+    int16_t height,
+    int16_t width,
+    uint16_t fill,
+    uint16_t background,
+    std::string text,
+    const GFXfont* font,
+    uint16_t colour,
+    BoxSides<uint8_t> padding,
+    VerticalDatum verticalDatum,
+    HorizontalDatum horizontalDatum,
+    uint8_t zIndex
+): Text(
+    x,
+    y,
+    height,
+    width,
+    fill,
+    background,
+    icon,
+    font,
+    colour,
+    padding,
+    verticalDatum,
+    horizontalDatum,
+    zIndex
+)
+{
+    this->setIcon(icon);
+};
+
+char Homelab::Graphics:Elements::Icon::getIcon() { return this->icon.set; }
+
+void Homelab::Graphics:Elements::Icon::setText(char icon) {
+    this->icon.set = icon;
+    this->setText(icon);
+}
+
+void Homelab::Graphics::Elements::Icon::draw(TFT_eSPI *tft) 
+{
+    Homelab::Graphics::Elements::Rectangle::draw(tft);
+    this->icon.displayed = this->icon.set
+}
+
+void Homelab::Graphics::Elements::Icon::loop(TFT_eSPI *tft) 
+{
+    if (
+        this->x.hasChanged() ||
+        this->y.hasChanged() ||
+        this->height.hasChanged() ||
+        this->width.hasChanged() ||
+        this->fill.hasChanged() ||
+        this->background.hasChanged() ||
+        this->text.hasChanged() ||
+        this->icon.hasChanged() ||
+        this->font.hasChanged() ||
+        this->colour.hasChanged() ||
+        this->padding.hasChanged() ||
+        this->verticalDatum.hasChanged() ||
+        this->horizontalDatum.hasChanged()
+    )
+    {
+        this->clear(tft);
+        this->draw(tft);
+    }
+};
---
+++ shared/c++/Homelab/Graphics/Elements/Icon/Icon.h
@@ -0,0 +1,42 @@
+#ifndef _Homelab_Graphics_Elements_Text_h
+#define _Homelab_Graphics_Elements_Text_h
+
+#include "../../Collision/Collision.h"
+
+#include "../../Collision/Collision.h"
+#include "../../structs.h"
+#include "../Text/Text.h"
+
+
+namespace Homelab::Graphics::Elements
+{
+    class Icon : public Homelab::Graphics::Elements::Text {
+        public:
+            DisplayedValue<char> icon;
+        
+            Icon(
+                int16_t x,
+                int16_t y,
+                int16_t height,
+                int16_t width,
+                uint16_t fill,
+                uint16_t background,
+                char icon,
+                const GFXfont* font,
+                uint16_t colour,
+                BoxSides<uint8_t> padding,
+                VerticalDatum verticalDatum,
+                HorizontalDatum horizontalDatum,
+                uint8_t zIndex
+            );
+
+            virtual char getIcon();
+
+            virtual void setIcon(char icon);
+
+            virtual void draw(TFT_eSPI *tft = nullptr);
+            virtual void loop(TFT_eSPI *tft = nullptr);
+    };
+}
+
+#endif
