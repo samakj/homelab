2fa0444178732b80906afdcc629432aa7811ced0
sam <sam@mollybet.com>
2022-09-20 15:50:17 +0100

    fix: Enable ssr styles

--- services/frontend/src/server.tsx
+++ services/frontend/src/server.tsx
@@ -5,6 +5,7 @@ import fs from 'fs';
 import path from 'path';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { ServerStyleSheet } from 'styled-components';
 import { App } from './App';
 
 const PORT = 8085;
@@ -13,15 +14,18 @@ const server = express();
 
 server.use(express.static(BUILD_DIR));
 server.use('*', (request, response) => {
-  const app = renderToString(<App location={request.originalUrl} />);
+  const sheet = new ServerStyleSheet();
+  const app = renderToString(sheet.collectStyles(<App location={request.originalUrl} />));
+  const styleTags = sheet.getStyleTags();
+  sheet.seal();
   const html = fs
     .readFileSync(path.resolve(BUILD_DIR, 'index.html'), { encoding: 'utf8' })
-    .replace('<div id="root"></div>', `<div id="root">${app}</div>`);
+    .replace('<div id="root"></div>', `<div id="root">${app}</div>`)
+    .replace('<!-- SSR_STYLE_TAGS -->', styleTags);
 
   // set header and status
   response.contentType('text/html');
   response.status(200);
-
   return response.send(html);
 });
 server.listen(PORT, () => console.log(`Express server started at http://localhost:${PORT}`));
