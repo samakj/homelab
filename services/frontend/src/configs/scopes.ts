b1482d16292764b19b364358c137e32f1096aded
sam <sam@mollybet.com>
2022-09-22 09:15:27 +0100

    fix: Add more scopes to config

--- services/frontend/src/configs/scopes.ts
+++ services/frontend/src/configs/scopes.ts
@@ -1,7 +1,28 @@
 /** @format */
 
 export const scopesMap = {
+  devices: {
+    get: 'devices.get',
+    create: 'devices.create',
+    update: 'devices.update',
+    delete: 'devices.delete',
+  },
   locations: {
     get: 'locations.get',
+    create: 'locations.create',
+    update: 'locations.update',
+    delete: 'locations.delete',
+  },
+  measurements: {
+    get: 'measurements.get',
+    create: 'measurements.create',
+    update: 'measurements.update',
+    delete: 'measurements.delete',
+  },
+  metrics: {
+    get: 'metrics.get',
+    create: 'metrics.create',
+    update: 'metrics.update',
+    delete: 'metrics.delete',
   },
 };
