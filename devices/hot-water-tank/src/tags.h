f5884b01cc78d498f3d0d2077a96ec78e475dc62
sam <sam@mollybet.com>
2022-10-13 17:20:59 +0100

    fix: Change hot water tank tag

--- devices/hot-water-tank/src/tags.h
+++ devices/hot-water-tank/src/tags.h
@@ -7,8 +7,8 @@
 
 std::vector<std::string> WIFI_SSID_TAGS = {"wifi", "ssid"};
 std::vector<std::string> WIFI_STRENGTH_TAGS = {"wifi", "strength"};
-std::vector<std::string> DHT_TEMPERATURE_TAGS = {"environment"};
-std::vector<std::string> DHT_HUMIDITY_TAGS = {"environment"};
+std::vector<std::string> DHT_TEMPERATURE_TAGS = {"cupboard"};
+std::vector<std::string> DHT_HUMIDITY_TAGS = {"cupboard"};
 std::vector<std::string> DS18B20_TEMPERATURE_TAGS = {"tank"};
 
 #endif
