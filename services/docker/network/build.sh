db8913aa1f092e904d4d0ecea92f853c93e061a5
Sam <sam@mollybet.com>
2026-01-15 09:08:17 +0000

    fix: Add cloudflare tunnel to top level run/build scripts

--- services/docker/network/build.sh
+++ services/docker/network/build.sh
@@ -1,6 +1,7 @@
 ROOT_DIR="${PWD%/homelab*}/homelab"
 
 . "${ROOT_DIR}/services/docker/network/authelia/build.sh"
+. "${ROOT_DIR}/services/docker/network/cloudflare-tunnel/build.sh"
 . "${ROOT_DIR}/services/docker/network/homepage/build.sh"
 . "${ROOT_DIR}/services/docker/network/traefik/build.sh"
 . "${ROOT_DIR}/services/docker/network/portainer/build.sh"
--- services/docker/network/run.sh
+++ services/docker/network/run.sh
@@ -1,6 +1,7 @@
 ROOT_DIR="${PWD%/homelab*}/homelab"
 
 . "${ROOT_DIR}/services/docker/network/authelia/run.sh"
+. "${ROOT_DIR}/services/docker/network/cloudflare-tunnel/run.sh"
 . "${ROOT_DIR}/services/docker/network/homepage/run.sh"
 . "${ROOT_DIR}/services/docker/network/traefik/run.sh"
 . "${ROOT_DIR}/services/docker/network/portainer/run.sh"
